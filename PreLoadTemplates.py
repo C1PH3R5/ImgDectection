b34d568d56b0737bf9f6f074a6341c101001af03
ubisgc <soeren.grimshave@gmail.com>
2021-02-16 23:03:18 +0100

    Merge change in branches to be redy for merg to master

--- PreLoadTemplates.py
+++ PreLoadTemplates.py
@@ -4,7 +4,7 @@ import glob
 class PreLoadTemplates:
     templates = []
     def loadTempates(self):
-       for file in glob.glob("./data/templates/*.jpg"):
+       for file in glob.glob("./data/templates/Test*.jpg"):
            self.templates.append(cv2.imread(file,0))
 
     def getTemplates(self):
